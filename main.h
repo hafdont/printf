578f5ea47058104041f9581c12513cdf1fd81899
Stephani6G <stephaniegloria283@gmail.com>
2023-09-20 19:40:57 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/*putchar function*/
+
+int _putchar(char c);
+
+/*string function*/
+int _puts(char *str);
+
+/* function that produces output according to a format*/
+int _printf(const char *format, ...);
+
+/*Handle the following conversion specifiers*/
+int handle_print(const char *fmt, int *i,
+va_list list, char buffer[], int flags, int width, int precision, int size);
+#endif
