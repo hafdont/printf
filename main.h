ab3cbbba88e1e1f5024c71ba683e82036a538a8f
Stephani6G <stephaniegloria283@gmail.com>
2023-09-20 21:01:49 -0700

    main.h

--- main.h
+++ main.h
@@ -18,4 +18,9 @@ int _printf(const char *format, ...);
 /*Handle the following conversion specifiers*/
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
+
+/*prints address of input in hexa form*/
+int print_address(va_list l, flags_t *f);
+
+
 #endif
